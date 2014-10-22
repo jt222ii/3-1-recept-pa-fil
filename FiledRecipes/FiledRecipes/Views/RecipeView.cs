eaffdcfafbbcdefb9ae301ab96230d3b6f161012
jt222ii <jt222ii@student.lnu.se>
2014-10-22 20:32:47 +0200

    Think i am done with RecipeView and Load() need to start and finish Save. Reverted the if-else into switches because something went wrong added a comment telling me to look into if again. Teacher said if was better but I did not get it to work for some reason.

--- FiledRecipes/FiledRecipes/Views/RecipeView.cs
+++ FiledRecipes/FiledRecipes/Views/RecipeView.cs
@@ -14,6 +14,25 @@ namespace FiledRecipes.Views
     {
         public void Show(IRecipe recipe)
         {
+            //Header, ShowHeader och ContinueOnKeyPressed
+            //Header får namnet av det valda receptet. ShowHeaderPanel(); använder sig av Header och skriver ut en panel med korrekt namn.
+            Header = recipe.Name;
+            ShowHeaderPanel(); //kallar på ShowHeaderPanel
+            //skriva ut recept - foreach som han visade på föreläsningen
+            //för varje ingrediens i ingredienser
+            Console.WriteLine("\nIngredienser\n------------");
+            foreach(Ingredient ingredient in recipe.Ingredients)
+            {
+                Console.WriteLine(ingredient);
+            }
+            //skriva ut instruktioner - foreach här också
+            //för varje instruktion i instruktioner
+            Console.WriteLine("\nGör Så Här\n----------");
+            foreach(string instruction in recipe.Instructions)
+            {
+                Console.WriteLine(instruction);
+            }
+
 
         }
         public void Show(IEnumerable<IRecipe> recipe)
